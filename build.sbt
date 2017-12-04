03f4d8ed0a752376e509e6f78eff02cfff8b363f
snaplucas <lucas.chapola@gmail.com>
2017-12-04 00:21:25 -0200

    +build.sbt and fix on wordCounter

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "Scala-Spark"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "2.2.0" % "provided"
--- src/main/scala/WordCounter.scala
+++ src/main/scala/WordCounter.scala
@@ -5,12 +5,12 @@ object WordCounter {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Word Counter")
     val sc = new SparkContext(conf)
-    val textFile = sc.textFile("path")
+    val textFile = sc.textFile("file:///usr/local/Cellar/apache-spark/2.2.0/README.md")
     val tokenizedFileData = textFile.flatMap(x => x.split(""))
     val countPrep = tokenizedFileData.map(x => (x, 1))
     val counts = countPrep.reduceByKey((x, y) => x + y)
     val sortedCounts = counts.sortBy(pair => pair._2, ascending = false)
-    sortedCounts.saveAsTextFile("another_path")
+    sortedCounts.saveAsTextFile("file:///Documents/WordCounter")
   }
 
 }
